3b94d65cf7a42fd0eb405b396852ba0a0fa03fe7
Xchel <Xchel@DESKTOP-F0KOPP8>
2023-07-30 02:38:04 -0600

    Clientes Agrupadores Limpiadores

--- src/main/java/com/dustbuster/dustbusterApi/Entity/Usuario.java
+++ src/main/java/com/dustbuster/dustbusterApi/Entity/Usuario.java
@@ -17,50 +17,66 @@ import java.util.List;
 @AllArgsConstructor
 @Table(name="Usuarios")
 public class Usuario {
-   /**
-    * NIVELES USUARIO
-    * ADMIN = 1
-    * LIMPIADOR = 2
-    * AGRUPADOR = 3
-    * CLIENTE = 4
-    *
-    * */
- @Id
+/**
+ * NIVELES USUARIO
+ * ADMIN = 1
+ * LIMPIADOR = 2
+ * AGRUPADOR = 3
+ * CLIENTE = 4
+ *
+ * */
+    @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_id")
     private Long UserId;
+
     @NotEmpty
     @Size(max = 100)
     @Column(name = "nombre_completo")
     private String nombreCompleto;
+
     @Size(max = 15)
-    @Column(name = "telefono")
-    private String telefono;
+    @Column(name = "numero_telefono")
+    private String numeroTelefono;
+
     @NotEmpty
     @Size(max = 100)
     @Column(name = "correo_electronico", unique = true)
     private String correo;
+
     @Column(nullable = false)
     @NotEmpty
     private String password;
     private Boolean enabled;
+
+
+    @Size(max = 200)
+    private String direccion;
     @Size(max = 200)
     private String ciudad;
     @Size(max = 200)
     private String estado;
     @Size(max = 200)
     private String curp;
-    private Long tipoUsuario;
+
+    @NotNull
+    @Column(name = "tipo_usuario")
+    private int tipoUsuario;
+
     @Size(max = 200)
     @Column(name = "foto_perfil")
     private String fotoPerfil;
+
     @Column(name = "fecha_registro", nullable = false, updatable = false, columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
     private Timestamp fechaRegistro;
+
     @Column(name = "ultima_sesion")
     private Timestamp ultimaSesion;
+
     @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "user_id", referencedColumnName = "user_id")
     private List<Rol> authorities;
+
     public Usuario() {
 
     }
